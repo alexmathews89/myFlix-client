6d80b3d1a48e5a08b67332eceb34734a7d843bc0
alexmathews89 <alexmathews@live.com>
2024-04-14 18:50:20 -0700

    created handleSubmit function

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 
 export const LoginView = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password,
+    };
+
+    fetch("https://myflixapp-495f4f3fbc03.herokuapp.com/login", {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+  };
+
   retrun(
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         Username:
         <input type="text" />
