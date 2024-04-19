9e8c8af3975d3031cdcaf17dbbcb8b4111064dc0
alexmathews89 <alexmathews@live.com>
2024-04-19 08:47:21 -0700

    tested app and updated prop types to match API object keys

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -7,16 +7,14 @@ export const MovieCard = ({ movie, onMovieClick }) => {
         onMovieClick(movie);
       }}
     >
-      {movie.title}
+      {movie.Title}
     </div>
   );
 };
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    director: PropTypes.string.isRequired,
-    genre: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired,
 };
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -3,15 +3,15 @@ export const MovieView = ({ movie, onBackClick }) => {
     <div>
       <div>
         <span>Title:</span>
-        <span>{movie.title}</span>
+        <span>{movie.Title}</span>
       </div>
       <div>
         <span>Director:</span>
-        <span>{movie.director}</span>
+        <span>{movie.Director.Name}</span>
       </div>
       <div>
         <span>Genre:</span>
-        <span>{movie.genre}</span>
+        <span>{movie.Genre.Name}</span>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
